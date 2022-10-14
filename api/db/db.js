e67bbd391d2bc17d461447a14f0e9cf7637444d0
Abdulaz1zov1 <oyatilloabdulazizov94@gmail.com>
2022-10-14 12:10:46 +0500

    tcti

--- api/db/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const connectDB = async () => {
-    mongoose.connect(
-          "mongodb://localhost:27017/ll"
-    )
-
-    console.log(`MongoDB Connected`)
-}
-module.exports = connectDB
